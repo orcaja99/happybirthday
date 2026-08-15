3883769061f6c0fb9c38eacae44a33c42e347cfb
orcaja99 <tealatte8@gmail.com>
2026-08-15 13:39:16 +0800

    tes

--- script.js
+++ script.js
@@ -302,7 +302,7 @@ window.addEventListener(
 
 const fireflies = document.getElementById("fireflies");
 
-for (let i = 0; i < 35; i++) {
+for (let i = 0; i < 25; i++) {
   const firefly = document.createElement("span");
 
   firefly.className = "firefly";
